a9259ca0f79d30920071b7a8728d5fdb12d3a602
JeppeOEM <jappa669@gmail.com>
2025-02-09 14:41:35 +0100

    nn

--- static/js/views/Home.js
+++ static/js/views/Home.js
@@ -1,5 +1,6 @@
 import AbstractView from './AbstractView.js';
 import { u } from '../lib.js';
+import '../components/popup-info-box-web-component/main.js'
 
 export default class extends AbstractView {
   constructor(params) {
@@ -19,6 +20,21 @@ export default class extends AbstractView {
 
         <br>
         <br>
+
+
+
+    <form>
+      <div>
+        <label for="cvc"
+          >Enter your CVC
+          <popup-info
+            img="img/alt.png"
+            data-text="Your card validation code (CVC) is an extra security feature — it is the last 3 or 4 numbers on the back of your card."
+          ></popup-info
+        ></label>
+        <input type="text" id="cvc" />
+      </div>
+    </form>
 
         <p>
           GitHub repo: <a href="https://github.com/michaelcurrin/single-page-app-vanilla-js">michaelcurrin/single-page-app-vanilla-js</a>.
